6dc7bbc299258bc7bb0a0c54e7463ca4c8116033
Yoandry Collazo <ycbarrosocu@gmail.com>
2025-07-03 10:57:13 -0600

    chore: update jest configuration to include moduleNameMapper for improved module resolution

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
-  testMatch: ['**/test/**/*.test.ts']
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
