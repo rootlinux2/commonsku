15473d7137cb4587f9ad9f955a5f479bc854e29d
Yoandry Collazo <ycbarrosocu@gmail.com>
2025-07-03 12:00:19 -0600

    feat: update Jest configuration for ESM support and modify test script

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,13 @@
+// jest.config.mjs
 export default {
-  preset: 'ts-jest/presets/default', // Use the correct ts-jest preset path
-  testEnvironment: 'node', // Set the test environment to Node.js
-  extensionsToTreatAsEsm: ['.ts'], // Treat TypeScript files as ES modules
-  moduleFileExtensions: ['ts', 'js'], // Recognize TypeScript and JavaScript files
-  testMatch: ['**/test/**/*.test.ts'], // Match test files in the specified directory
-  transformIgnorePatterns: [
-    '/node_modules/(?!(@octokit)/)', // Ensure @octokit is transformed
-  ],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Map .js imports to their corresponding files without the extension
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
   },
-  rootDir: './', // Set the root directory for Jest
+  transformIgnorePatterns: ['/node_modules/(?!(@octokit)/)'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['**/test/**/*.test.ts'],
 };
